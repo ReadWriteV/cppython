741a6558496a020603c419570df15d7729477fe1
ReadWriteV <ReadWriteV@outlook.com>
2024-01-07 16:22:42 +0800

    cleanup

--- src/runtime/cell.cpp
+++ src/runtime/cell.cpp
@@ -1,21 +1,15 @@
 #include "runtime/cell.hpp"
 
-// #include "memory/oopClosure.hpp"
 #include "object/dict.hpp"
 #include "object/string.hpp"
 
 using namespace cppython;
 
 cell_klass::cell_klass() {
-  set_dict(std::make_shared<dict>());
   set_name("cell");
+  set_dict(std::make_shared<dict>());
 }
 
 cell::cell(std::shared_ptr<list> t, int i) : table{t}, index{i} {
   set_klass(cell_klass::get_instance());
 }
-
-// void CellKlass::oops_do(OopClosure *closure, HiObject *obj) {
-//   CellObject *co = (CellObject *)obj;
-//   closure->do_oop((HiObject **)&co->_table);
-// }
--- src/runtime/cell.hpp
+++ src/runtime/cell.hpp
@@ -10,21 +10,15 @@
 namespace cppython {
 
 class object;
-class list;
 
 class cell_klass : public klass, public singleton<cell_klass> {
   friend class singleton<cell_klass>;
 
 private:
   cell_klass();
-
-public:
-  //   virtual void oops_do(OopClosure *closure, HiObject *obj);
-  virtual size_t size() { return sizeof(cell_klass); };
 };
 
 class cell : public object {
-  //   friend class cell_klass;
 public:
   cell(std::shared_ptr<list> t, int i);
   std::shared_ptr<object> value() {
