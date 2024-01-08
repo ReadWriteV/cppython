5855016d53ed1b3afecaa70a77489a8aee5e3656
ReadWriteV <ReadWriteV@outlook.com>
2024-01-08 21:50:09 +0800

    math lib

--- src/extlib/math.cpp
+++ src/extlib/math.cpp
@@ -1,30 +1,35 @@
 #include "inc/cppython.hpp"
+#include "object/float.hpp"
+#include "object/integer.hpp"
 
 #include <cmath>
+#include <vector>
 
 using namespace cppython;
 
 // issue: static value in cppython.exe and math.dll
 
-double get_double(vector<object *> *args) {
+double get_double(std::shared_ptr<std::vector<std::shared_ptr<object>>> args) {
   auto x = args->at(0);
   double y = 0;
-  if (x->is<integer>()) {
-    y = x->as<integer>()->get_value();
-  } else if (x->is<float_num>()) {
-    y = x->as<float_num>()->get_value();
+  if (x->get_klass() == integer_klass::get_instance()) {
+    y = std::static_pointer_cast<integer>(x)->get_value();
+  } else if (x->get_klass() == float_klass::get_instance()) {
+    y = std::static_pointer_cast<float_num>(x)->get_value();
   }
   return y;
 }
 
-object *math_sqrt(vector<object *> *args) {
+std::shared_ptr<object>
+math_sqrt(std::shared_ptr<std::vector<std::shared_ptr<object>>> args) {
   double x = get_double(args);
-  return new float_num{std::sqrt(x)};
+  return std::make_shared<float_num>(std::sqrt(x));
 }
 
-object *math_sin(vector<object *> *args) {
+std::shared_ptr<object>
+math_sin(std::shared_ptr<std::vector<std::shared_ptr<object>>> args) {
   double x = get_double(args);
-  return new float_num{std::sin(x)};
+  return std::make_shared<float_num>(std::sin(x));
 }
 
 ext_method math_methods[] = {{.method_name = "sin",
--- src/inc/cppython.hpp
+++ src/inc/cppython.hpp
@@ -1,17 +1,10 @@
 #pragma once
 
-#include "object/float.hpp"
-#include "object/integer.hpp"
 #include "runtime/function.hpp"
 
-#include <memory>
 #include <string_view>
-#include <vector>
 
 namespace cppython {
-
-using native_function_t = std::shared_ptr<object>(
-    std::shared_ptr<std::vector<std::shared_ptr<object>>>);
 
 struct ext_method {
   std::string_view method_name;
